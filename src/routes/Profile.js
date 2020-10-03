ecaea703f5bceff7c4dbe05b8aaaf3f3650b220b
nam20 <43489607+nam20@users.noreply.github.com>
2020-10-03 17:58:53 +0900

    nwitter

---
+++ src/routes/Profile.js
@@ -0,0 +1,64 @@
+import { authService, dbService } from 'fBase';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default ({ userObj, refreshUser }) => {
+
+    const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    const onChange = (event) => {
+        const {
+            target : { value },
+        } = event;
+        setNewDisplayName(value);
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName){
+            await userObj.updateProfile({
+                displayName : newDisplayName,
+            })
+            refreshUser();
+        }
+    }
+    const getMyNweets = async () => {
+        const nweets = await dbService
+            .collection("nweets")
+            .where("creatorId", "==", userObj.uid)
+            .orderBy("createdAt")
+            .get();
+            console.log(nweets.docs.map(doc => doc.data()));
+    }
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+    return (
+        <div className="container">
+          <form onSubmit={onSubmit} className="profileForm">
+              <input 
+                type="text" 
+                placeholder="Display name" 
+                onChange={onChange} 
+                value={newDisplayName}
+                autoFocus
+                className="formInput"
+              />
+              <input
+                type="submit"
+                value="Update Profile"
+                className="formBtn"
+                style={{
+                    marginTop: 10,
+                }}
+              />
+          </form>
+          <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+            Log Out
+          </span>
+        </div>
+    )
+}
